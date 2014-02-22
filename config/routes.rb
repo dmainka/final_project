424a6a0781ed04f9a02f7dd6ab379c0027d45e7c
dmainka <dmainka@gmail.com>
2014-02-22 16:26:25 -0600

    Finish basic static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 FinalProject::Application.routes.draw do
 
   # Home page
-  root "products#home"
+  root  'static_pages#home'
+
+  get "static_pages/home", :as => 'home'
+  get "static_pages/help", :as => 'help'
+  get "static_pages/about", :as => 'about'
+  get "static_pages/contact", :as => 'contact'
+
+  # Home page
+  # root "products#home"
 
 
 
