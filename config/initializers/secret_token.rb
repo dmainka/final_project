89a9d14a6fe662477e3eb474da1c14f711294b36
dmainka <dmainka@gmail.com>
2014-02-22 07:46:39 -0600

    Start Customers

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FinalProject::Application.config.secret_key_base = '00ac9283901f18ad9db88375697511cd955f3cc13106dd52418f1e15c8c8f10c72622e6414488191403b5dec3696d1b1b44f80031204f52ead53a2d7690971f8'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+FinalProject::Application.config.secret_key_base = secure_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FinalProject::Application.routes.draw do
+  get "customers/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
