005629d10e368e28aa65576bf8e470d118525b74
dmainka <dmainka@gmail.com>
2014-02-22 17:45:15 -0600

    Finish signup

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,11 @@
 module CustomersHelper
+
+  # Returns the Gravatar (http://gravatar.com/) for the given customer.
+  def gravatar_for(customer, options = { size: 50 })
+    gravatar_id = Digest::MD5::hexdigest(customer.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: customer.name, class: "gravatar")
+  end
+
 end
